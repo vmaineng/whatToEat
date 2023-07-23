5f831963b5f22d0f7f72749a7f9d57e0c795caac
Mai Vang <vmaineng@gmail.com>
2023-07-23 16:36:33 -0700

    added in system role

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 import { config } from "dotenv";
-config()
+config();
 
-import { Configuration, OpenAIApi } from "openai"
+import { Configuration, OpenAIApi } from "openai";
 
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.API_KEY,
+  })
+);
 
-
-console.log(process.env.API_KEY)
+openai
+  .createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You will be provided with questions, and your task to is generate a response",
+      },
+      {
+        role: "user",
+        content:
+          "Recommend me some places to eat when my girlfriend states she doesn't know what to eat?",
+      },
+    ],
+  })
+  .then((res) => {
+    console.log(res);
+  });
